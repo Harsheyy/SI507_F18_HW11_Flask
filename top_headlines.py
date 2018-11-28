9d76fe7d2af8aec9284c91799911ac471e1e855f
Harsh K Desai <harshkdesai109@yahoo.com>
2018-11-28 11:30:56 -0500

    Final commit

--- top_headlines.py
+++ top_headlines.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 from datetime import datetime
 import secrets
 import requests
 
-api_key = secrets.api_key
+nyt_key = secrets.api_key
 
 app = Flask(__name__)
 
@@ -22,13 +22,32 @@ elif(hour > 16 and hour < 21):
 else:
 	greeting = 'Good night'
 
+def get_stories(section):
+	baseurl ='https://api.nytimes.com/svc/topstories/v2/' 
+	extendedurl = baseurl + section +'.json' 
+	params={'api-key': nyt_key}
+	return requests.get(extendedurl, params).json()
+
+section ='technology'
+stories = get_stories(section)
+headlines = []
+
+for i in range(len(stories['results'])):
+	if(stories['results'][i]['section'] == 'Technology'):
+		article = stories['results'][i]['title']
+		link = stories['results'][i]['short_url']
+		headlines.append(str(article) + ' (' + str(link) + ')')
+
+top5 = headlines[:5]
+
 @app.route('/')
 def index():    
     return '<h1>Welcome!</h1>'
 
 @app.route('/user/<name>')
 def user(name):
-	return '<h1>%s, %s!</h1>' % (greeting, name)
+	return render_template('user.html',GREET = greeting ,NAME = name, article_list = top5)
+	pass
 
 if __name__ == '__main__':  
     print('starting Flask app', app.name)  
